4134942c6afaa01bdf6aa2399cb233c9f2a704eb
xgro <zero1401@naver.com>
2023-03-31 19:02:51 +0900

    Feat. cleanup producer code

--- handler_producer.py
+++ handler_producer.py
@@ -7,33 +7,30 @@ API_APP_ID = os.getenv('API_APP_ID')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
 
 def lambda_handler(event, context):
-    slack_body  = event['body']
-    slack_event = json.loads(slack_body)
-    
-    if "challenge" in slack_event:
-        challenge_answer = slack_event.get("challenge")
-        print(challenge_answer)
-        return { 'statusCode': 200, 'body': challenge_answer }
-    
-    # if not slack_event.get("api_app_id"):
-    #     raise ValueError 
-    
-    # if API_APP_ID != slack_event.get("api_app_id"):
-    #     raise ValueError
-    
-    # if slack_event.get("event").get("channel") != CHANNEL_ID:
-    #     raise ValueError
-    
-    SQS = boto3.client('sqs')
-    # print(team_id)
-    SQS.send_message(
-        QueueUrl=QUEUE_URL,
-        MessageBody=slack_body
-    )
+    try:
+        slack_body  = event['body']
+        slack_event = json.loads(slack_body)
+        
+        if "challenge" in slack_event:
+            challenge_answer = slack_event.get("challenge")
+            print(challenge_answer)
+            return { 'statusCode': 200, 'body': challenge_answer }
+        
+        SQS = boto3.client('sqs')
+        SQS.send_message(
+            QueueUrl=QUEUE_URL,
+            MessageBody=slack_body
+        )
 
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': ""
-    }
+        # TODO implement
+        return {
+            'statusCode': 200,
+            'body': ""
+        }
+        
+    except:
+        return {
+            'statusCode': 400,
+            'body': ""
+        }
 
